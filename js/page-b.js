9b41eacff4c0fb1fea0b7a382d98853b13ef432c
naraku777 <442815900@qq.com>
2017-07-22 15:52:59 +0800

    6-1 Boy action decomposition

--- js/page-b.js
+++ js/page-b.js
@@ -1,8 +1,75 @@
-//
+// js/page-b.js
 
-function PageB(callback){
-  //模拟执行时间
-	setTimeout(function() {
-		callback()
-	}, 2000)
+
+function PageB(element, pageComplete) {
+
+  var ANIMATION_END = 'animationend webkitAnimationEnd'
+
+  // boy
+  var $boy = element.find('.xmas-boy')
+
+  // boy action
+  var boyAction = {
+    walk: function () {
+      var dfd = $.Deferred()
+      $boy.transition({
+        "right": "4.5rem"
+      }, 4000, "linear", function () {
+        dfd.resolve()
+      })
+      return dfd
+    },
+    stopWalk: function () {
+      $boy.removeClass('boy-walk')
+      $boy.addClass('boy-stand')
+    },
+    runWalk: function () {
+      $boy.addClass('walk-run')
+    },
+    unwrap: function () {
+      var dfd = $.Deferred()
+      $boy.addClass('boy-unwrap')
+      $boy.removeClass('boy-stand')
+      $boy.one(ANIMATION_END, function () {
+        dfd.resolve()
+      })
+      return dfd
+    },
+    undress: function (count) {
+      $boy.addClass('boy-undress-step-' + count).removeClass('boy-unwrap')
+    },
+    hug: function () {
+      $boy.addClass('boy-hug').one(ANIMATION_END, function () {
+        $('.xmas-boy-head').show()
+      })
+    }
+  }
+
+  // start action
+  boyAction
+    // start walk
+    .walk()
+    // stop walk
+    .then(function () {
+      boyAction.stopWalk()
+    })
+    // unwrap package
+    .then(function () {
+      boyAction.unwrap()
+    })
+    // undress
+    .then(function () {
+      setTimeout(function () {
+        boyAction.undress(1)
+      }, 1000)
+      setTimeout(function () {
+        boyAction.undress(2)
+      }, 2000)
+      setTimeout(function () {
+        boyAction.undress(3)
+      }, 3000)
+      setTimeout(function () {
+        boyAction.hug()
+      }, 4000)
+    })
 }
--- js/xmas.js
+++ js/xmas.js
@@ -5,73 +5,75 @@
 // 模拟镜头效果切换页面
 function changePage(element, effect, callback) {
 
-    element
-        .addClass(effect)
-        .one('animationend webkitAnimationEnd', function () {
-            callback && callback()
-        })
+  element
+    .addClass(effect)
+    .one('animationend webkitAnimationEnd', function () {
+      callback && callback()
+    })
 }
 
 // Background music
 function H5Audio(url, loop) {
-    var audio = new Audio(url)
+  var audio = new Audio(url)
 
-    audio.autoplay = true
-    audio.loop = loop || false
-    audio.play()
+  audio.autoplay = true
+  audio.loop = loop || false
+  audio.play()
 
-    return {
-        end: function (callback) {
-            audio.addEventListener('ended', function () {
-                callback && callback()
-            }, false)
-        }
+  return {
+    end: function (callback) {
+      audio.addEventListener('ended', function () {
+        callback && callback()
+      }, false)
     }
+  }
 }
 
 var Xmas = function () {
-    var $pageA = $('.page-a')
-    var $pageB = $('.page-b')
-    var $pageC = $('.page-c')
+  var $pageA = $('.page-a')
+  var $pageB = $('.page-b')
+  var $pageC = $('.page-c')
 
-    // // 观察者
-    // var observer = new Observer()
-    // // Page A
-    // new PageA(function () {
-    //   observer.publish('completeA')
-    // })
-    // // Enter page B
-    // observer.subscribe('pageB', function () {
-    //   new PageB(function () {
-    //     observer.publish("completeB")
-    //   })
-    // })
-    // // Enter page C
-    // observer.subscribe('pageC', function () {
-    //   new PageC()
-    // })
+  // // 观察者
+  // var observer = new Observer()
+  // // Page A
+  // new PageA(function () {
+  //   observer.publish('completeA')
+  // })
+  // // Enter page B
+  // observer.subscribe('pageB', function () {
+  //   new PageB(function () {
+  //     observer.publish("completeB")
+  //   })
+  // })
+  // // Enter page C
+  // observer.subscribe('pageC', function () {
+  //   new PageC()
+  // })
 
-    // // Page A to page B
-    // observer.subscribe('completeA', function () {
-    //   changePage($pageA, 'effect-out', function () {
-    //     observer.publish('pageB')
-    //   })
-    // })
-    // // Page B to page C
-    // observer.subscribe('completeB', function () {
-    //   changePage($pageC, 'effect-in', function () {
-    //     observer.publish('pageC')
-    //   })
-    // })
+  // // Page A to page B
+  // observer.subscribe('completeA', function () {
+  //   changePage($pageA, 'effect-out', function () {
+  //     observer.publish('pageB')
+  //   })
+  // })
+  // // Page B to page C
+  // observer.subscribe('completeB', function () {
+  //   changePage($pageC, 'effect-in', function () {
+  //     observer.publish('pageC')
+  //   })
+  // })
 
-    new PageA($pageA)
+  // new PageA($pageA)
+
+  new PageB($pageB)
 }
 
 
 $(function () {
-    Xmas()
-    // var bgAudio = H5Audio('audio/scene.mp3')
-    // bgAudio.end(function () {
-    //   console.log('Background music ended...')
-    // })
+  Xmas()
+  // var bgAudio = H5Audio('audio/scene.mp3')
+  // bgAudio.end(function () {
+  //   console.log('Background music ended...')
+  // })
 })
